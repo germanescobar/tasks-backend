293533a6758c870029d16dd7505e2f784e225990
Juan <juanan0309@gmail.com>
2021-09-07 16:14:44 -0500

    solve conflicts

--- tasksController.js
+++ tasksController.js
@@ -1,32 +1,21 @@
 const store = require("./store");
 
 const list = (req, res) => {
-<<<<<<< HEAD
-  res.json(store.list());
+  store.list().then((tasks) => res.json(tasks));
 };
-
-const create = (req, res) => {
-  const task = store.create({ title: req.body.title });
-  res.status(201).json(task);
-};
-=======
-  store.list()
-    .then(tasks => res.json(tasks))
-}
 
 const create = async (req, res, next) => {
   try {
-    const task = await store.create({ title: req.body.title })
-    res.status(201).json(task)
+    const task = await store.create({ title: req.body.title });
+    res.status(201).json(task);
   } catch (err) {
     if (err.name === "ValidationError") {
-      res.status(422).json(err.errors)
+      res.status(422).json(err.errors);
     } else {
-      next(err)
+      next(err);
     }
   }
-}
->>>>>>> ec9a2118cab2c7cb60e0f181bf748372efeefff2
+};
 
 const destroy = (req, res) => {
   store.delete(req.params.id);
