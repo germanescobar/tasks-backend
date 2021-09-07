20e2a15ed3cea44c2cf5b19e308e8712c6cbbd7b
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2021-09-07 18:24:19 -0500

    Finish challenge

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-const express = require("express");
-const cors = require("cors");
-const store = require("./store");
-const routes = require("./routes");
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const store = require('./store');
+const routes = require('./routes');
+const User = require('./user.model');
 const app = express();
 
 // middlewares - chain of responsability
 app.use(cors());
 app.use(express.json()); // parsea el body a JSON
 
+const uri =
+  'mongodb+srv://Cesar-Diaz:vacivusVoid1%40@cluster0.ybria.mongodb.net/Agora?retryWrites=true&w=majority';
+
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established succesfully');
+});
+
+async function run() {
+  try {
+    // crear un documento
+    // const user = new User({
+    //   email: 'david@example.com',
+    //   password: 'test1234',
+    //   firstName: 'David',
+    //   lastName: 'Rodriguez',
+    // });
+    // await user.save(); // guarde en la base de datos
+    /*await Post.create({ userId: "613233fb5f971766ecd37366", title: "Tercer post", content: "Contenido del tercer post",
+    tags: ["mongodb", "code"]})*/
+    // actualizar un documento
+    // eliminar un documento
+    /*const post = await Post.findOne({ _id: "61376fe376f579016904d9e7" })
+    await post.remove()*/
+    // await Post.deleteOne({ _id: "61376fe376f579016904d9e7" })
+    // listar documentos
+    // const results = await Post.find()
+    // console.log(results)
+    // const user = await User.authenticate("david@example.com", "testddd1234")
+    // console.log("User: ", user)
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      console.log('Error de validación:', err.errors);
+    } else {
+      console.log(err);
+    }
+  }
+}
+
+run();
 // rutas - endpoints - controladores
 app.use(routes);
 
@@ -17,7 +60,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message });
 });
 
-app.listen(3001, () => console.log("Servidor corriendo ..."));
+app.listen(3001, () => console.log('Servidor corriendo ...'));
 
 // AJAX (Asynchronous JavaScript And XML) - llamados del cliente a servidor
 // Google Maps
--- middlewares.js
+++ middlewares.js
@@ -1,30 +1,38 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const User = require('./user.model');
 
 const logger = (req, res, next) => {
   // autenticación, trazabilidad
-  console.log("Llegó una nueva petición")
-  next()
-}
+  console.log('Llegó una nueva petición');
+  next();
+};
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   try {
-    const token = req.get('Authorization')
-    const data = jwt.verify(token, 'secret key') // { userId: "12345"}
-    console.log("Data del token:", data)
-    console.log("UserId: ", data.userId)
-    
+    const token = req.get('Authorization');
+    const data = jwt.verify(token, 'secret key'); // { userId: "12345"}
+    console.log('Data del token:', data);
+    console.log('UserId: ', data.userId);
+
     // cargar el usuario a partir del userId que llega en el token
-    next()
-  } catch (err) {
-    if (err.name === "JsonWebTokenError") {
-      res.status(401).json({ error: "Invalid Token" })
-      return
+    const user = await User.findOne({ _id: data.userId });
+    console.log(user);
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ error: 'Invalid Token' });
+      return;
     }
-    next(err)
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError') {
+      res.status(401).json({ error: 'Invalid Token' });
+      return;
+    }
+    next(err);
   }
-}
+};
 
 module.exports = {
   logger,
-  auth
-}
+  auth,
+};
